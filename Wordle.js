a1e551ff3bccbc462d410403eda80fa73eb61352
Cameron-Griff <71991702+Cameron-Griff@users.noreply.github.com>
2022-02-28 18:51:49 -0500

    Update Wordle.js

--- Wordle.js
+++ Wordle.js
@@ -9,22 +9,22 @@ let guess = upperguess.toUpperCase()
 let review = []
 
 //Spliting characters into seperate array spots
-let arr = word.split('');
-let bruh = guess.split('');
+let word_array = word.split('');
+let guess_array = guess.split('');
 //Print array
-console.log(`Guess: ${bruh}`); // Should print "Guess: PURSE"
-console.log(`Word: ${arr}`); // Delete this for implementation, just to test code! Should print "Word: PULSE"
+console.log(`Guess: ${guess_array}`); // Should print "Guess: PURSE"
+console.log(`Word: ${word_array}`); // Delete this for implementation, just to test code! Should print "Word: PULSE"
 //Identifying shared letters between guess and message
-const shared = bruh.some(cam => arr.includes(cam))
-const letters = bruh.filter(cam => arr.includes(cam))
+const shared = guess_array.some(cam => word_array.includes(cam))
+const letters = guess_array.filter(cam => word_array.includes(cam))
 console.log(`The guess and word share the following letters: ${letters}`)
 
 
 
-for (index = 0; index < arr.length; index++) { //Looking through all characters of message and seeing if character is in correct spot. If it is, place 1, if not place 0.
-  if (bruh[index] === arr[index]) {
+for (index = 0; index < word_array.length; index++) { //Looking through all characters of message and seeing if character is in correct spot. If it is, place 1, if not place 0.
+  if (guess_array[index] === word_array[index]) {
     review[index]='1'
-  } else if (bruh[index] != arr[index]) {
+  } else if (guess_array[index] != word_array[index]) {
     review[index]='0'
   }
 }
